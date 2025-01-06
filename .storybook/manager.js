c4bc6b867db820acd2606860458e25cd227383d1
Ella-aaa <ella@sooho.io>
2025-01-06 19:31:50 +0900

    sooho 테마 추가

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from "@storybook/manager-api";
-import { themes } from "@storybook/theming";
+import soohoTheme from "./soohoTheme";
 
 addons.setConfig({
-  theme: themes.dark,
+  theme: soohoTheme,
 });
